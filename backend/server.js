ac8b31eed2cd054fd59d11fce848ba51747468e7
Balram Dhakad <bdhakad886@gmail.com>
2024-03-24 20:04:34 +0530

    version 1.5

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,13 @@ dotenv.config();
 connectDB();
 job.start();
 
-const PORT = process.env.PORT || 5000;
-
 app.use(cors({
 	origin: ["https://gossip-api.vercel.app/", "https://gossips-bd.vercel.app"],
-	methods: ["POST", "GET"],
+	methods: ["POST", "GET", "PUT", "DELETE"],
 	credentials: true
 }));
+
+const PORT = process.env.PORT || 5000;
 
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -37,6 +37,5 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-// http://localhost:5000 => backend,frontend
 
 server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
