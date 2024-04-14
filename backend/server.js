f89c52c5aef925cdd777b657745bdf472d7774cf
balram2002 <162151085+balram2002@users.noreply.github.com>
2024-04-14 15:59:13 +0530

    version 5.2

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
