bc72ac776718f94d81a6491614a6c88b65e54dc0
Balram Dhakad <bdhakad886@gmail.com>
2025-07-18 01:42:16 +0530

    version 7.2

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "https://gossip-bd.vercel.app",
+		origin: ["https://gossip-bd.vercel.app", "https://gossip-api.vercel.app"],
 		methods: ["GET", "POST"],
 	},
 });
