b1f023d069aec9b708c4308a7463258bfbd20363
Balram Dhakad <bdhakad886@gmail.com>
2025-07-17 20:24:59 +0530

    version 6.7

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import protectRoute from "../middlewares/protectRoute.js";
+// import protectRoute from "../middlewares/protectRoute.js";
 import { getMessages, sendMessage, getConversations } from "../controllers/messageController.js";
 
 const router = express.Router();
 
-router.get("/conversations", protectRoute, getConversations);
-router.get("/:otherUserId", protectRoute, getMessages);
-router.post("/", protectRoute, sendMessage);
+router.get("/conversations", getConversations);
+router.get("/:otherUserId", getMessages);
+router.post("/", sendMessage);
 
 export default router;
